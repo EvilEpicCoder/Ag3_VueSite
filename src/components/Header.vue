ff7f2b69d61e3ee487551dc0e014d0aa22afe69d
EvilEpicCoder <1st.elixile@gmail.com>
2017-11-18 21:45:24 +0300

    ipApi function added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,9 @@
 <template>
-<div> {{ title }} </div>
+<div>
+  <div> {{ geodata.code }} </div>
+  <div> {{ geodata.city }} </div>
+  <div> {{ geodata.country }} </div>
+</div>
 </template>
 
 <script>
@@ -11,6 +15,11 @@ export default {
       user: {
         firstname: 'Igor',
         lastname: 'Gavelyuk'
+      },
+      geodata: {
+        code: 'code is unset',
+        country: 'country is unset',
+        city: 'city is unset'
       }
     }
   },
@@ -18,7 +27,24 @@ export default {
     is_working: function () {
       alert('is working ok')
     },
-    test: () => alert('test ok')
+    test: () => alert('test ok'),
+    ipApi: (superKey) => {
+      var xhttp
+      xhttp = new XMLHttpRequest()
+      xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+          let tgeo = JSON.parse(this.responseText)
+          superKey.code = tgeo.country_code
+          superKey.country = tgeo.country_name
+          superKey.city = tgeo.city
+        }
+      }
+      xhttp.open('GET', 'https://freegeoip.net/json/', true)
+      xhttp.send()
+    }
+  },
+  beforeMount () {
+    this.ipApi(this.geodata)
   }
 }
 </script>
